b179f5986f01f584ad8719a834a280df0ab7659b
zhaimengli <1178604898@qq.com>
2018-05-27 20:58:02 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+   <Header v-bind:title="title" v-bind:leftoptionsshowBack="true" left-options.backtext=true   v-on:cancel="alert(1)"/>
+</template>
+
+<script>
+import Header from "../components/common/header"
+
+export default {
+  components:{
+    Header
+  },
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      title:"主界面"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  background:#333;
+  color:#fff;
+  height:40px;
+  line-height:40px;
+  text-align:center
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
